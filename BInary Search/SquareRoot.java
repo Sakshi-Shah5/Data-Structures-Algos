7e2261a84490693da4e3da1f01dee030e4e5e29b
Sakshi Shah <67853646+Sakshi-Shah5@users.noreply.github.com>
2022-08-30 20:00:57 +0530

    Create SquareRoot.java

---
+++ BInary Search/SquareRoot.java
@@ -0,0 +1,34 @@
+//find square root of x  //leet 69
+//(You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5)
+
+public class SquareRoot {
+	
+	static int squareRoot(int x) {
+		int low = 1;
+		int high = x/2;
+		int ans=0;
+		
+		while(low <= high) {
+			int mid = low + (high-low)/2;
+			int square = mid * mid;
+			if(square > x) {
+				high = mid-1;
+			}
+			else if(square < x) {
+				ans = mid;
+				low = mid+1;
+			}
+			else {
+				return mid;
+			}
+		}
+		return ans;
+		
+	}
+	
+	public static void main(String[] args) {
+		int x = 120;
+		System.out.print(squareRoot(x));
+	}
+
+}

e824ecd1264f18604dee4538b5dd0f083a2c65df
Sakshi Shah <67853646+Sakshi-Shah5@users.noreply.github.com>
2022-09-08 12:45:36 +0530

    Create ReverseLL.java

---
+++ LinkedList/ReverseLL.java
@@ -0,0 +1,49 @@
+package leetcode_problems;
+
+public class ReverseLL {
+	
+	public static ListNode reverse(ListNode head) {
+		
+		ListNode current = head;
+		ListNode previous= null;
+		ListNode next = null;
+		
+		while(current != null) {
+			next = current.next;
+			current.next = previous;
+			previous = current;
+			current= next;
+		}
+		
+		return previous;
+	}
+	
+	public static void display(ListNode head) {
+		ListNode current = head;
+		
+		while(current != null) {
+			System.out.print(current.val + " --> ");
+			current = current.next;
+		}
+		System.out.println("null");
+	}
+	
+	public static void main(String args[]) {
+		ListNode head = new ListNode(0);
+		ListNode first = new ListNode(1);
+		ListNode second = new ListNode(2);
+		ListNode third = new ListNode(3);
+		
+		head.next = first;
+		first.next = second;
+		second.next = third;
+		third.next = null;
+		
+		display(head);
+		ListNode temp = reverse(head);
+		display(temp);
+		
+		
+	}
+
+}
